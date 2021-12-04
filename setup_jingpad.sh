6a91fa5722f790a4ddb95af139a57591f84dcf1e
Travis McLane <tmclane@gmail.com>
2021-12-04 09:47:00 -0600

    add -y to apt install & add Golang from longsleep PPA

--- setup_jingpad.sh
+++ setup_jingpad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install openssh-server wget curl git emacs \
+sudo apt install -y openssh-server wget curl git emacs \
      cairo-dev python3-cairo-dev \
      libcairo2-dev python3-dev \
      python3-key-mapper python-pip3 \
@@ -14,10 +14,18 @@ cp -r key-mapper-config ~/.config/key-mapper
 #sudo pip3 install --no-binary :all: git+https://github.com/tmclane/key-mapper.git
 cd key-mapper
 ./scripts/build.sh
-sudo apt install ./dist/key-mapper-1.2.1.deb
+sudo apt install -y ./dist/key-mapper-1.2.1.deb
 sudo sed -i -e "s/USER/$(whoami)/g" /etc/xdg/autostart/key-mapper-autoload.desktop
 sudo systemctl enable key-mapper
 sudo systemctl restart key-mapper
 
 # activate the mapping now so we don't have to wait for a reboot
 key-mapper-control --command autoload --config-dir /home/$(whoami)/.config/key-mapper
+
+# Install PPA Packages
+# Install Go
+# Download PPA Public Key
+sudo apt-key adv --keyserver keyserver.ubuntu.com --receive-keys F6BC817356A3D45E
+echo "deb http://ppa.launchpad.net/longsleep/golang-backports/ubuntu focal main\ndeb-src http://ppa.launchpad.net/longsleep/golang-backports/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/golang.list
+sudo apt update
+sudo apt install -y golang-go
